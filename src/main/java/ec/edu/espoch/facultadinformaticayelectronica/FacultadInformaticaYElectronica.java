18868d592c75a75e81ef7c9ae86950d105fff842
jonnierpl10 <jonnierpilozo@gmail.com>
2024-11-25 16:57:25 -0500

    Facultad de Informatica

--- src/main/java/ec/edu/espoch/facultadinformaticayelectronica/FacultadInformaticaYElectronica.java
+++ src/main/java/ec/edu/espoch/facultadinformaticayelectronica/FacultadInformaticaYElectronica.java
@@ -12,5 +12,6 @@ public class FacultadInformaticaYElectronica {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Bienvenidos a la Facultad de Informatica y Electronica");
     }
 }
